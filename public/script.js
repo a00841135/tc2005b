5dd51568b5e30e63599ad79a14d3dd6954a55a8e
ARandomDoggo <a00841135@tec.mx>
2026-04-10 21:51:35 -0600

    back+front

--- public/script.js
+++ public/script.js
@@ -7,25 +7,53 @@ const btnLogin = document.getElementById("btn-login");
 
 btnLogin.addEventListener("click", handleLogin);
 
-function handleLogin() {
+async function handleLogin() {
     let isValid = true;
 
-    usernameError.textContent = "";
-    passwordError.textContent = "";
+    clearFormErrors();
 
-    if (usernameInput.value.trim() === "") {
+    let usernameIsEmpty = usernameInput.value.trim() === "";
+    let passwordIsEmpty = passwordInput.value.trim() === "";
+
+    if (usernameIsEmpty) {
         usernameError.textContent = "Username cannot be empty.";
         isValid = false;
     }
 
-    if (passwordInput.value.trim() === "") {
+    if (passwordIsEmpty) {
         passwordError.textContent = "Password cannot be empty.";
         isValid = false;
     }
 
     if (isValid) {
-        alert("Logged in successfully!");
-        usernameInput.value = "";
-        passwordInput.value = "";
+        const credentials = {username: usernameInput.value, password: passwordInput.value};
+
+        const res = await fetch("http://localhost:5000/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(credentials)});
+        
+        const data = await res.json();
+        console.log(data);
+
+        clearFormInputs();
+
+        if (data.isLogin === true) { 
+            sessionStorage.setItem("id", data.user.id);
+            sessionStorage.setItem("username", data.user.username);
+            window.location = "./about.html"
+        } else {
+            alert("wrong....");
+        }
     }
+}   
+
+function clearFormInputs() {
+    usernameInput.value = "";
+    passwordInput.value = "";
+}
+
+function clearFormErrors() {
+    usernameError.textContent = "";
+    passwordError.textContent = "";
 }
